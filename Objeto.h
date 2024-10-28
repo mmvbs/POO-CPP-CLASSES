b7ec1821b5c47bcbd0b53f9e041e021033b6813f
MichelyFelix <michelyvitoriauzl@gmail.com>
2024-10-28 18:01:03 -0300

    objeto

---
+++ Objeto.h
@@ -0,0 +1,26 @@
+// Objeto.h
+#ifndef OBJETO_H
+#define OBJETO_H
+
+#include <string>
+
+class Objeto {
+public:
+    Objeto(float largura, float altura, const std::string& tela, const std::string& sprite_nome, 
+           float velocidade, float x_inicial, float y_inicial, float largura_sprite, float altura_sprite);
+
+    void load_images(const std::string& sprite_nome, float largura_sprite, float altura_sprite);
+    void draw();
+    void update();
+    void rect();// Função paar susbtituir a função rect do pygame
+protected:
+    float largura, altura;
+    std::string tela;
+    float velocidade;
+    float x, y;
+    void* image; 
+
+    int getImageWidth(); // Função vazia para obter a largura da imagem
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include<iostream>
+#include"Objeto.h"
 using namespace std;
 
+#define largura = 1280
+#define altura = 720
 int main(){
     cout << "CYG em cpp" << endl;
+    
     return 0;
 }
