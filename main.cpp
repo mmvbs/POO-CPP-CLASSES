ae1023fa7fe17792a8b46e53c717f12ac0277390
mM-b <gtamsue@gmail.com>
2024-10-31 15:36:12 -0300

    final

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main() {
     cout << "CYG em cpp" << endl;
 
     Moita moita(LARGURA, ALTURA, "tela_exemplo", "moita.png", 1, 800, 600, 100, 100);
-    Tumulos tumulo(LARGURA, ALTURA, "tela_exemplo", "tumulo.png", 1, 1, 600, 100, 100);
+    Tumulos tumulo(LARGURA, ALTURA, "tela_exemplo", "tumulo.png", 1, 700, 600, 100, 100);
     
     moita.load_images("moita.png", 100, 100);
     for (int i = 0; i < 2; ++i) {
